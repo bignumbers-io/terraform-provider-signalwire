32254e30a2438264b900af216a7930402a1a6b0c
Tim Coulson <tim.r.coulson@gmail.com>
2020-04-20 19:20:39 +0100

    feat: implement SIP endpoint resource

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/bignumbers-io/terraform-provider-signalwire/provider"
-	"github.com/hashicorp/terraform/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
 func main() {
